b78f4dcf172509d4db487c585f6708af81824635
Dawood Faisal <dawood_faisal@yahoo.com>
2024-07-25 19:20:08 +0500

    commit-2 JavaScript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("This is my script")
+let result = {
+    
+        "tag": "",
+        "free": true,
+        "role": false,
+        "user": "dawood_faisal",
+        "email": "dawood_faisal@yahoo.com",
+        "score": 0.64,
+        "state": "deliverable",
+        "domain": "yahoo.com",
+        "reason": "valid_mailbox",
+        "mx_found": true,
+        "catch_all": null,
+        "disposable": false,
+        "smtp_check": true,
+        "did_you_mean": "",
+        "format_valid": true
+      
+}
+let str = ``
+for (key of Object.keys(result)){
+    str = str + `<div>${key}: ${result[key]}</div>`
+}
+console.log(str)
