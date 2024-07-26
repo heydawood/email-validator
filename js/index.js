28957f7f633d69192d79124b8c991069e0bbd93b
Dawood Faisal <dawood_faisal@yahoo.com>
2024-07-26 16:51:26 +0500

    Final commit with Js updated

--- js/index.js
+++ js/index.js
@@ -1,24 +1,43 @@
 let result = {
+
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "dawood_faisal",
+    "email": "dawood_faisal@yahoo.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "yahoo.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+
+}
+
+fetch
+
+submitBtn.addEventListener("click", async(e) => {
+    e.preventDefault()
     
-        "tag": "",
-        "free": true,
-        "role": false,
-        "user": "dawood_faisal",
-        "email": "dawood_faisal@yahoo.com",
-        "score": 0.64,
-        "state": "deliverable",
-        "domain": "yahoo.com",
-        "reason": "valid_mailbox",
-        "mx_found": true,
-        "catch_all": null,
-        "disposable": false,
-        "smtp_check": true,
-        "did_you_mean": "",
-        "format_valid": true
-      
-}
-let str = ``
-for (key of Object.keys(result)){
-    str = str + `<div>${key}: ${result[key]}</div>`
-}
-console.log(str)
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="loading"></img>`
+    let key = "ema_live_Wsy3nNOu7584kcu58VjIyiMGTbIKWKXLntofnnbU"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        //if(result[key] !== "" && result[key] == " "){
+
+            str = str + `<div>${key}: ${result[key]}</div>`
+       // }
+    }
+
+    console.log("str")
+    resultCont.innerHTML = str
+
+})
